480528da5ee4e42eb6e25f604616a12c7f9c3c61
Michel <michelribeirocorrea187@gmail.com>
2026-01-29 00:46:04 -0300

    feat: add custom exception classes and resource files for validation errors

---
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyRecipeBook.Exceptions.ExceptionsBase;
+
+public class ErrorOnValidationException : MyRecipeBookException
+{
+  public IList<string> ErrorMessages { get; set; }
+
+  public ErrorOnValidationException(IList<string> errors)
+  {
+    ErrorMessages = errors;
+  }
+
+}
